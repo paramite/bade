b869b568b71244fc56b7fe54bd5e923011a688f7
Martin Mágr <mmagr@redhat.com>
2014-09-24 13:04:14 +0200

    Puppetfile parser
    This patch add PuppeFile class which will be used to read/modify Puppetfiles.

--- bade/utils.py
+++ bade/utils.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import click
-import collections
 import logging
 import os
 import pipes
@@ -74,14 +73,75 @@ def shout(msg, verbose=False, nl=True, level='info'):
 
 class PuppetFile(object):
     """Puppetfile parser"""
-    _content = collections.OrderedDict()
+    _content = {}
 
-    def load(self, source):
-        """Loads modules information from Puppetfile located
-        in directory 'source'.
-        """
+    def __init__(self, repo):
+        self._fpath = os.path.join(
+            os.path.abspath(repo),
+            'Puppetfile'
+        )
 
-    def save(self, destination):
-        """Saves modules information to Puppetfile to directory
-        'destination'.
-        """
+    def __getitem__(self, key):
+        return self._content[key]
+
+    def __setitem__(self, key, value):
+        self._content[key] = value
+
+    def __contains__(self, item):
+        return item in self._content
+
+    def __iter__(self):
+        return iter(self._content.keys())
+
+    def keys(self):
+        return self._content.keys()
+
+    def values(self):
+        return self._content.values()
+
+    def items(self):
+        return self._content.items()
+
+    def load(self, source=None):
+        """Loads modules information from Puppetfile 'source'."""
+        # TO-DO: Implement smarter parser
+        re_head = re.compile(
+            r'^mod\s+["\'](?P<name>[\w\-]+)["\']\s*,\s*\n$'
+        )
+        re_info = re.compile(
+            r'\s+\:(?P<key>\w+)'
+                r'\s*=>\s*'
+            r'["\'](?P<value>[\w\-\/\.\:]+)["\']\s*,?\s*\n$'
+        )
+        fpath = source or self._fpath
+        with open(fpath) as puppetfile:
+            mod = None
+            for line in puppetfile:
+                match = re_head.match(line)
+                if match:
+                    mod = match.group('name')
+                    continue
+                match = re_info.match(line)
+                if match:
+                    mod_dict = self._content.setdefault(mod, {})
+                    key = match.group('key')
+                    value = match.group('value')
+                    mod_dict[key] = value
+
+    def save(self, destination=None):
+        """Saves modules information to Puppetfile 'destination'."""
+        fmt_head = "mod '{name}',\n"
+        fmt_info = "  :{key} => '{value}'"
+
+        fpath = destination or self._fpath
+        with open(fpath, 'w') as puppetfile:
+            for module in sorted(self._content.keys()):
+                puppetfile.write(fmt_head.format(name=module))
+                keys = self._content[module].keys()
+                for key in sorted(self._content[module].keys()):
+                    value = self._content[module][key]
+                    puppetfile.write(
+                        fmt_info.format(key=key, value=value)
+                    )
+                    keys.remove(key)
+                    puppetfile.write(',\n' if keys else '\n\n')
